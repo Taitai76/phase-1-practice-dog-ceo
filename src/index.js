3354bd7cb3b8da9e89ca91be25b9933d988d9ebb
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-06-12 16:58:55 -0400

    commit tree 2ef3e65038dc79c79d83b034d902952462af3db6

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('%c HI', 'color: firebrick')
+const list =  document.querySelector("dog-image-container");
+console.log(list);
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(r => r.json())
+.then(data => {
+    console.log(data);
+    list.innerHTML=`
+    <img src=${data} alt="Italian Trulli">
+    `
+})
